d4ada8d14401a3ee36b07619724ccd983c802ead
Hedi Gardi <hedi.gardi@medieinstitutet.se>
2024-01-21 23:39:26 +0100

    updated courses & db

--- scripts/courses.js
+++ scripts/courses.js
@@ -2,13 +2,13 @@ import { createCard, addImageClickHandler } from './dom.js';
 import HttpClient from './http.js';
 import { navAnimation } from "./nav.js";
 
-const gallery = document.querySelector('#course-container');
+const allCourses = document.querySelector('#course-container');
 
 async function initPage() {
   const courses = await loadCourses();
 
   courses.forEach((course) => {
-    gallery.appendChild(createCard(course));
+    allCourses.appendChild(createCard(course));
   });
 
   const images = document.querySelectorAll('.course-card img');
